a193c4d79d6988c11a1a900e45e5688505acdd8c
Rafaelly Lemos <rafaelly@zexter.com>
2026-03-20 11:25:27 -0300

    chore: boilerplate setup with vite and tailwind v4

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-3xl ml-2 font-light text-blue-700/70 shadow-lg">Let's do that!</h1>   
+    </div>
+
+
+  )
+}
+
+export default App
